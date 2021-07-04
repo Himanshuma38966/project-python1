939b10a21f7d3b2a448b9b2f15101ec951ce1522
Himanshuma38966 <74598469+Himanshuma38966@users.noreply.github.com>
2021-07-04 11:53:58 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,21 @@
+import random
+
+ran = random.randint(1,10)
+number = int(input("enter the number"))
+
+
+
+
+if (number <ran):
+    print("you lost! the number is wrong")
+
+
+
+
+if (number ==ran):
+    print("you won! the number is correct")
+
+
+if (number> ran):
+    print("you lost! the number is wrong")
+
